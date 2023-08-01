081e51b35c0b1eab1cc76c2311664297bd33cb27
Ethan Lim <etlim@u91z01s06.42kl.edu.my>
2023-08-01 14:53:14 +0800

    Changes made in norm

--- libft/ft_printf.c
+++ libft/ft_printf.c
@@ -6,11 +6,39 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:13:03 by etlim             #+#    #+#             */
-/*   Updated: 2023/07/26 16:08:06 by etlim            ###   ########.fr       */
+/*   Updated: 2023/08/01 14:48:48 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+// #include "stdio.h"
+
+void	ft_checkchar(va_list args, const char format, int *total)
+{
+	if (format == 'c')
+		ft_printchar(va_arg(args, int), total);
+	else if (format == 's')
+		ft_printstr(va_arg(args, char *), total);
+	else if (format == 'i' || format == 'd')
+		ft_printnum((long long int)va_arg(args, int), "0123456789", total);
+	else if (format == 'u')
+		ft_printnum((long int)va_arg(args, unsigned int), "0123456789", total);
+	else if (format == 'x')
+		ft_printnum((long long int)va_arg(args, unsigned long int),
+			"0123456789abcdef", total);
+	else if (format == 'p')
+	{
+		ft_printchar('0', total);
+		ft_printchar('x', total);
+		ft_printnum_unsigned((long int)va_arg(args, unsigned long int),
+			"0123456789abcdef", total);
+	}
+	else if (format == 'X')
+		ft_printnum((long int)va_arg(args, unsigned long int),
+			"0123456789ABCDEF", total);
+	else if (format == '%')
+		ft_printchar('%', total);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -25,41 +53,14 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == 'c')
-				ft_printchar(va_arg(args, int), &total);
-			else if (format[i + 1] == 's')
-				ft_printstr(va_arg(args, char *), &total);
-			else if (format[i + 1] == 'i' || format[i + 1] == 'd')
-				ft_printnum((long long int)va_arg(args, int),
-					"0123456789", &total);
-			else if (format[i + 1] == 'u')
-				ft_printnum((long int)va_arg(args, unsigned int),
-					"0123456789", &total);
-			else if (format[i + 1] == 'x')
-			{
-				ft_printnum((long long int)va_arg(args, unsigned long int),
-					"0123456789abcdef", &total);
-			}
-			else if (format[i + 1] == 'p')
-			{
-				total += write(1, "0x", 2);
-				ft_printnum_unsigned((long int)va_arg(args, unsigned long int),
-					"0123456789abcdef", &total);
-			}
-			else if (format[i + 1] == 'X')
-			{	
-				ft_printnum((long int)va_arg(args, unsigned long int),
-					"0123456789ABCDEF", &total);
-			}
-			else if (format[i + 1] == '%')
-				ft_printchar('%', &total);
+			ft_checkchar(args, format[i + 1], &total);
 			i++;
 		}
 		else
 			ft_printchar(format[i], &total);
 	}
-	return (total);
 	va_end(args);
+	return (total);
 }
 
 // int main(void)
@@ -67,6 +68,7 @@ int	ft_printf(const char *format, ...)
 // 	int mine;
 // 	int original;
 // 	char *str = "banana";
+
 // 	original = printf("start %d %% end\n", 2147483647);
 // 	mine = ft_printf("start %d %% end\n", 2147483647);
 // 	printf("my count: %i, original count: %i\n", mine, original);
--- libft/ft_printnum.c
+++ libft/ft_printnum.c
@@ -6,28 +6,17 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 15:17:39 by etlim             #+#    #+#             */
-/*   Updated: 2023/07/25 18:02:10 by etlim            ###   ########.fr       */
+/*   Updated: 2023/08/01 14:49:18 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/**
- * @brief this function prints numbers whether it be in decimal, 
- * integer, hexadecimal , or pointer
- * 
- * @param num 
- * input
- * @param hex 
- * format of the numbers to be printed out
- * @param count 
- * total count
- */
 
-// for pointer and other unsigned just call this resursion
 void	ft_printnum_unsigned(unsigned long long int num, char *hex, int *count)
 {
-	int	len = ft_strlen(hex);
-	
+	int	len;
+
+	len = ft_strlen(hex);
 	if (num > 0)
 		ft_printnum(num / len, hex, count);
 	ft_printchar(hex[num % len], count);
@@ -35,13 +24,14 @@ void	ft_printnum_unsigned(unsigned long long int num, char *hex, int *count)
 
 void	ft_printnum(long long int num, char *hex, int *count)
 {
-	int	len = ft_strlen(hex);
-	// check if number is positive or negative
+	int	len;
+
+	len = ft_strlen(hex);
 	if (num < 0)
 	{
 		ft_printchar('-', count);
-		num = num * -1; // chenge to positive
-		ft_printnum(num, hex, count); // call printnum
+		num = num * -1;
+		ft_printnum(num, hex, count);
 	}
 	else
 	{
